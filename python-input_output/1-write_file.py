4d37bc5c1461d9016e0afbbca4ef2beb51e3bea2
Erjok2020 <e.mach@alustudent.com>
2022-07-29 08:10:51 -0700

    add

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A function that write into a file"""
+
+
+def write_file(filename="", text=""):
+    """Write a file"""
+
+    with open(filename, "w") as file:
+        return file.write(text)
