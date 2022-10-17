e08196d4563333aede7c5392371859cde075486b
Erjok2020 <e.mach@alustudent.com>
2022-10-17 04:55:40 -0700

    neww

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,23 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
-
-request(process.argv[2], function (error, response, body) {
+const fs = require('fs');
+const args = process.argv.slice(2);
+request(args[0], (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const data = JSON.parse(body);
-    const movieCount = data.results.length;
-    let finalResult = 0;
-    // const comString = 'https://swapi-api.hbtn.io/api/people/18/';
-    for (let i = 0; i < movieCount; i++) {
-      const numChars = data.results[i].characters.length;
-      for (let j = 0; j < numChars; j++) {
-        const comString0 = data.results[i].characters[j];
-        if (comString0.slice(-4, -1) === '/18') {
-          finalResult += 1;
-        }
-      }
-    }
-    console.log(finalResult);
+    fs.writeFile(args[1], body, 'utf8', (error) => {
+      if (error) console.log(error);
+    });
   }
 });
